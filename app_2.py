d5694adeb8819f5e4c215be1bd82ab1fcf2ff96c
Halimatu Abubakar <154516107+halimatu608@users.noreply.github.com>
2025-03-04 16:35:50 +0100

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -22,7 +22,7 @@ final_score = st.number_input('Final Score', min_value=0, max_value=100, step=1)
 if st.button('Predict'):
     user_input = np.array([[math_score, reading_score, writing_score, final_score]])
     prediction = model.predict(user_input)
-    species_mapping = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'F'}
+    performance_mapping = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'F'}
     
     predicted_student_performance = performance_mapping.get(int(prediction[0]), 'unknown')
     st.write(f'The predicted species is: {predicted_student_performance}')
