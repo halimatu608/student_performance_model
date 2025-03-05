7d6d42b91d6c4ea215b03be7beb62aadfb2f5441
Halimatu Abubakar <154516107+halimatu608@users.noreply.github.com>
2025-03-05 14:31:40 +0100

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -6,6 +6,9 @@ import numpy as np
 with open('first_student_performance_model.pkl', 'rb') as file:
     model= pickle.load(file)
 
+with open("scaler.pkl", "rb") as scale_file:
+    scaler=pickle.load(scale_file)
+
 # streamlit UI 
 
 st.title('Student Performance Prediction App')
@@ -21,7 +24,8 @@ gender_male_female = st.number_input('Gender', min_value=0, max_value=1, step=1)
 
 if st.button('Predict'):
     user_input = np.array([[math_score, reading_score, writing_score, gender_male_female]])
-    prediction = model.predict(user_input)
+    scaled=scaler.transform(user_input)
+    prediction = model.predict(scaled)
     
     performance_mapping = {0: 'A - Excellent',1: 'B - Very Good',2: 'C - Good',3: 'D - Pass',4: 'F - Fail'}
     
